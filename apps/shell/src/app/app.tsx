efc5e8ff3daf5f6cf8114569944e5c7005653767
Cristia Medina <cdmedina229@gmail.com>
2025-05-18 15:08:38 -0500

    base

---
+++ apps/shell/src/app/app.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import NxWelcome from './nx-welcome';
+import { Link, Route, Routes } from 'react-router-dom';
+
+const Signin = React.lazy(() => import('signin/Module'));
+const Signup = React.lazy(() => import('signup/Module'));
+const Ticketlist = React.lazy(() => import('ticketlist/Module'));
+const Ticketcreate = React.lazy(() => import('ticketcreate/Module'));
+
+export function App() {
+  return (
+    <React.Suspense fallback={null}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/signin">Signin</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+        <li>
+          <Link to="/ticketlist">Ticketlist</Link>
+        </li>
+        <li>
+          <Link to="/ticketcreate">Ticketcreate</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<NxWelcome title="shell" />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/ticketlist" element={<Ticketlist />} />
+        <Route path="/ticketcreate" element={<Ticketcreate />} />
+      </Routes>
+    </React.Suspense>
+  );
+}
+
+export default App;
